60c52cc48696a1d4fc4616eb8a53538c9831a63b
unknown <hieu.tran@mrisoftware.com>
2022-06-03 16:26:37 -0400

    generate csv
    Signed-off-by: unknown <hieu.tran@mrisoftware.com>

--- layout_model.py
+++ layout_model.py
@@ -4,6 +4,7 @@ import os
 from azure.ai.formrecognizer import DocumentAnalysisClient
 from azure.core.credentials import AzureKeyCredential
 from dotenv import load_dotenv
+import csv
 
 load_dotenv()
 # set `<your-endpoint>` and `<your-key>` variables with the values from the Azure portal
@@ -17,7 +18,7 @@ def format_bounding_box(bounding_box):
 
 def analyze_layout():
     # sample form document
-    formUrl = "https://filestorage4321.blob.core.windows.net/source/lord_nelson_place(single).pdf"
+    formUrl = "https://filestorage4321.blob.core.windows.net/source/{}.pdf".format(os.environ.get('FILE'))
     # create your `DocumentAnalysisClient` instance and `AzureKeyCredential` variable
     document_analysis_client = DocumentAnalysisClient(endpoint=endpoint, credential=AzureKeyCredential(key))
 
@@ -25,45 +26,55 @@ def analyze_layout():
             "prebuilt-layout", formUrl)
     result = poller.result()
 
-    for idx, style in enumerate(result.styles):
-        print(
-            "Document contains {} content".format(
-                "handwritten" if style.is_handwritten else "no handwritten"
-            )
-        )
+    # for idx, style in enumerate(result.styles):
+    #     print(
+    #         "Document contains {} content".format(
+    #             "handwritten" if style.is_handwritten else "no handwritten"
+    #         )
+    #     )
 
-    for page in result.pages:
-        for line_idx, line in enumerate(page.lines):
-            print(
-            "...Line # {} has text content '{}'".format(
-            line_idx,
-            line.content.encode("utf-8")
-            )
-        )
+    # for page in result.pages:
+    #     for line_idx, line in enumerate(page.lines):
+    #         print(
+    #         "...Line # {} has text content '{}'".format(
+    #         line_idx,
+    #         line.content.encode("utf-8")
+    #         )
+    #     )
 
-        for selection_mark in page.selection_marks:
-            print(
-            "...Selection mark is '{}' and has a confidence of {}".format(
-            selection_mark.state,
-            selection_mark.confidence
-            )
-        )
+    #     for selection_mark in page.selection_marks:
+    #         print(
+    #         "...Selection mark is '{}' and has a confidence of {}".format(
+    #         selection_mark.state,
+    #         selection_mark.confidence
+    #         )
+    #     )
 
-    for table_idx, table in enumerate(result.tables):
-        print(
-            "Table # {} has {} rows and {} columns".format(
-            table_idx, table.row_count, table.column_count
-            )
-        )
-            
-        for cell in table.cells:
-            print(
-                "...Cell[{}][{}] has content '{}'".format(
-                cell.row_index,
-                cell.column_index,
-                cell.content.encode("utf-8"),
-                )
-            )
+    with open('{}.csv'.format(os.environ.get('FILE')), 'w') as csvfile:
+        writer = csv.writer(csvfile)
+    # for table_idx, table in enumerate(result.tables):
+    #     print(
+    #         "Table # {} has {} rows and {} columns".format(
+    #         table_idx, table.row_count, table.column_count
+    #         )
+    #     )
+        for table_idx, table in enumerate(result.tables):
+    # for cell in table.cells:
+    #     print(
+    #         "...Cell[{}][{}] has content '{}'".format(
+    #         cell.row_index,
+    #         cell.column_index,
+    #         cell.content.encode("utf-8"),
+    #         )
+    #     )
+            row = []
+            for cell in table.cells:
+                if cell.column_index < (table.column_count - 1):
+                    row.append(cell.content)
+                else:
+                    row.append(cell.content)
+                    writer.writerow(row)
+                    row = []
 
     print("----------------------------------------")
 
